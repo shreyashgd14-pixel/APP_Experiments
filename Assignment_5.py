ffb8b9c5f50042629fc1d417b00c0e433268e0e8
shreyashgd14-pixel <ADT25SOCB1251@students.mituniversity.edu.in>
2026-09-04 17:56:40 +0530

    Create Assignment_5.py

---
+++ Assignment_5.py
@@ -0,0 +1,25 @@
+def lcs(X, Y):
+    m = len(X)
+    n = len(Y)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[i - 1] == Y[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    i, j = m, n
+    result = []
+    while i > 0 and j > 0:
+        if X[i - 1] == Y[j - 1]:
+            result.append(X[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+    return ''.join(reversed(result))
+X = "AGAB"
+Y = "GXTXB"
+print("Longest Common Subsequence:", lcs(X, Y))
