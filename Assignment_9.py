4b8c8e337ecd46e08331096e8d09bedec8feb96d
shreyashgd14-pixel <ADT25SOCB1251@students.mituniversity.edu.in>
2026-09-24 11:34:51 +0530

    Create Assignment_9.py

---
+++ Assignment_9.py
@@ -0,0 +1,11 @@
+import csv
+import json
+
+file_1= with open("student.csv","r")
+file_2= with open("students.json","r")
+data=["raj",3,10]
+csv_reader= csv.reader(file_1)
+for row in csv_reader:
+  data.append(row)
+json_file = json.reader(file_2)
+json.dump(data,json_file)
